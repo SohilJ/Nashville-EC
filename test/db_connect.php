7cececc74f9aaf363ab15896992cc421a3c3910b
Sohil Jain <sohil147@gmail.com>
2020-08-05 21:48:31 -0400

    frontend

---
+++ test/db_connect.php
@@ -0,0 +1,73 @@
+<?php
+
+$dbhost = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'database_db';
+$con = mysqli_connect("$dbhost", "$username", "$password");
+
+mysqli_connect("$dbhost", "$username", "$password");
+
+//echo "Connected";
+
+mysqli_select_db($con, $db);
+
+//echo "Connected2";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    $query = $_GET['query']; 
+    // gets value sent over search form
+     
+    $min_length = 3;
+    // you can set minimum length of the query if you want
+     
+    if(strlen($query) >= $min_length){ // if query length is more or equal minimum length then
+         
+        $query = htmlspecialchars($query); 
+        // changes characters used in html to their equivalents, for example: < to &gt;
+         
+        $query = mysqli_real_escape_string($con, $query);
+        // makes sure nobody uses SQL injection
+         
+        $raw_results = mysqli_query($con, "SELECT users.name FROM users, user_demographics, demographics WHERE demographics.demographic LIKE '%".$query."%' AND user_demographics.d_id_FK= demographics.d_id AND users.id= user_demographics.user_id_FK") or die(mysqli_error($con));
+             
+        // * means that it selects all fields, you can also write: `id`, `title`, `text`
+        // articles is the name of our table
+         
+        // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
+        // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
+        // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
+         
+        if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+             
+            while($results = mysqli_fetch_array($raw_results)){
+            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+             
+                echo "<p><h3>".$results['name']."</h3>".$results['description']."</p>";
+                // posts results gotten from database(title and text) you can also show id ($results['id'])
+            }
+             
+        }
+        else{ // if there is no matching rows do following
+            echo "No results";
+        }
+         
+    }
+    else{ // if query length is less than minimum
+        echo "Minimum length is ".$min_length;
+    }
+?>
+
+</body>
+</html>
---
+++ test/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="db_connect.php" method="GET">
+    <input type="text" name="query" />
+    <input type="submit" value="Search" />
+</form>
+
+</body>
+</html>
